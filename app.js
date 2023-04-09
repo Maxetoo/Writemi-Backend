083a60fa49454703b00917a9f6946a95d767300b
Ayoade David <adewoleayoade057@gmail.com>
2023-04-09 07:54:38 +0100

    cookie config test

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ const swaggerDocument = YAML.load('./swagger.yaml')
 
 // middlewares
 
-/*app.use(
+app.use(
   cors({
     credentials: true,
     origin: '*',//'https://writemi-frontend.vercel.app',
   })
-)*/
+)
 app.use(fileUploader({ useTempFiles: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
